0a47041c9ea566cb4ddb6c30f7e9f9c113f65d6d
tankTopTaro <balmoresk3v1n@gmail.com>
2024-10-08 06:25:48 +0800

    remove routers

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,10 @@
-import { Link, useLocation, useNavigate } from "react-router-dom"
-
 const Navbar = () => {
   const location = useLocation()
 
   return (
     <div className="probootstrap-bar">
-      {location.pathname === '/single' ? (
-        <Link className="probootstrap-refresh" to='/'><span className="oi oi-arrow-left"></span></Link>
-      ) : (
         <div className="probootstrap-refresh"><span className="oi oi-reload"></span></div>
-      )}
-        <a href="#" className="probootstrap-toggle js-probootstrap-toggle"><span className="oi oi-menu"></span></a>
+        <div className="probootstrap-toggle js-probootstrap-toggle"><span className="oi oi-menu"></span></div>
         <div className="probootstrap-main-site-logo"><a href="index.html">Photo</a></div>
     </div>
   )
